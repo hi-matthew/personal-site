57391cab95fce0f1868dce30d1a699cc0ee19fc8
Matthew Martinez <hellomattmartinez@gmail.com>
2018-12-03 13:56:50 -0700

    adjust blog post heading for mobile

---
+++ src/components/blogHeader.js
@@ -0,0 +1,72 @@
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+const Header = styled.div`
+  position: fixed;
+  color: ${props => props.theme.darkblack};;
+  background-color: white;
+  height: 70px;
+  left: 50vw;
+  overflow: hidden;
+  top: 0;
+  right: 0;
+  z-index: 2;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px 30px 0;
+  box-sizing: border-box;
+  font-weight: 800;
+  font-size: 15px;
+  opacity: 0.9;
+  transition: box-shadow 300ms ease;
+  box-shadow: ${props => props.active
+    ? `1px 0px 5px ${props.theme.lightblack}`
+    : null
+    };
+  @media (max-width: 750px) {
+    top: 0%;
+    position: absolute;
+    left: 0;
+  }
+  .link {
+    text-decoration: none;
+    color: ${props => props.theme.darkblack};
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    margin-top: 12px;
+  }
+  .link::after {
+    height: 7px;
+    width: 0;
+    left: 50%;
+    right: 50%;
+    margin-top: 5px;
+    background-color: ${props => props.theme.teal};
+    content: '';
+    position: relative;
+    transition: left 300ms ease, right 300ms ease, width 300ms ease;
+  }
+  .link:hover::after {
+    left: 0;
+    right: 0;
+    width: 100%;
+    transition: left 300ms ease, right 300ms ease, width 300ms ease;
+  }
+`
+
+const BlogHeader = (props) => (
+  <Header active={props.activeMenu}>
+    <Link
+    to={'/prose'}
+    className="link"
+    >
+      Back to listings
+    </Link>
+    {props.composedDate}
+  </Header>
+)
+
+export default BlogHeader;
--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 import styled from "styled-components"
 import HeroImg from "../components/heroImg"
+import BlogHeader from "../components/blogHeader"
 import HeroShell, { HeroText } from "../components/heroStyles"
 import Layout from "../components/layout"
 
@@ -41,59 +42,6 @@ const Text = styled.div`
   color: ${props => props.theme.darkblack};
   p {
     color: ${props => props.theme.darkblack};
-  }
-`
-const Header = styled.div`
-  position: fixed;
-  color: ${props => props.theme.darkblack};;
-  background-color: white;
-  height: 70px;
-  left: 50vw;
-  overflow: hidden;
-  right: 0;
-  z-index: 2;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 20px 30px 0;
-  box-sizing: border-box;
-  font-weight: 800;
-  font-size: 15px;
-  opacity: 0.9;
-  transition: box-shadow 300ms ease;
-  box-shadow: ${props => props.active
-    ? `1px 0px 5px ${props.theme.lightblack}`
-    : null
-    };
-  @media (max-width: 750px) {
-    top: 0%;
-    position: absolute;
-    left: 0;
-  }
-  .link {
-    text-decoration: none;
-    color: ${props => props.theme.darkblack};
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    margin-top: 12px;
-  }
-  .link::after {
-    height: 7px;
-    width: 0;
-    left: 50%;
-    right: 50%;
-    margin-top: 5px;
-    background-color: ${props => props.theme.teal};
-    content: '';
-    position: relative;
-    transition: left 300ms ease, right 300ms ease, width 300ms ease;
-  }
-  .link:hover::after {
-    left: 0;
-    right: 0;
-    width: 100%;
-    transition: left 300ms ease, right 300ms ease, width 300ms ease;
   }
 `
 
@@ -145,17 +93,16 @@ export default class blogPost extends Component {
         <HeroShell>
           <HeroImg page={location.pathname}/>
           <HeroText>{post.frontmatter.title}</HeroText>
+          {window.innerWidth > 750
+          ? <BlogHeader activeMenu={activeMenu} composedDate={composedDate} />
+          : null
+          }
         </HeroShell>
         <TextContainer>
-          <Header active={activeMenu}>
-            <Link
-            to={'/prose'}
-            className="link"
-            >
-              Back to listings
-            </Link>
-            {composedDate}
-          </Header>
+          {window.innerWidth < 751
+          ? <BlogHeader activeMenu={activeMenu} composedDate={composedDate} />
+          : null
+          }
           <Text dangerouslySetInnerHTML={{ __html: post.html }} />
           {!showComposedDate
           ? <span className='alternate-date'>
